d4e83c8250251b2893c6b1feee8db338ff23382b
Matthias B <brett_vorm_kopf@gmx.de>
2015-12-13 15:32:56 +0100

    Update of the main function.

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main()
 {
-    printf("Hello world!\n");
+/***************** GLOBAL VARIABLES **********************/
+    uint8_t field[11][11];
+
+
+/***************** PROGRAMM CODE *************************/
+
+    printf("Field(s) starting screen!\n");
+
+
+
     return 0;
 }
 
